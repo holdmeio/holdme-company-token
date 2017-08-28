d76d0140bbba1fff531946515946da54a5c0ddec
IAM DEV <elky@iamdeveloper.io>
2017-08-28 21:31:21 +0200

    Constructor changed

--- contracts/Holdme.sol
+++ contracts/Holdme.sol
@@ -11,19 +11,16 @@ contract Holdme is Ownable, Standard23Token {
   bytes32 public symbol = "HME";
   uint256 public decimals = 18;
 
-  //1 ether = 1 HME
-  uint256 public constant PRICE = 1 ether;
-
   address public owner;
 
 
-  function XPOSE(address _centralAdmin){
+  function Holdme(address _centralAdmin){
     if (_centralAdmin != 0){
       owner = _centralAdmin;
     } else {
       owner = msg.sender;
     }
-    totalSupply = 10000000 * 10**18;
+    totalSupply = 300000000 * 10**18;
     balances[msg.sender] = totalSupply;
 
   }
