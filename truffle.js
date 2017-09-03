a804acf89ffa4177699e0406edc7f065b7f1ef08
Gilang Bhagaskara <sciters@gmail.com>
2017-09-03 23:17:11 +0700

    Reconfigured truffle deployment network targets

--- truffle.js
+++ truffle.js
@@ -2,7 +2,18 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ropsten_elky: {
+      host: "localhost",
+      port: 8545,
+      from: "0x0bE9FC0FC5d2696edF93F9256F6871217695B4B6",
+      network_id: "*" // Match any network id
+    },
+    ropsten_gilang: {
+      host: "localhost",
       port: 8545,
       from: "0x18d54cca8608d90661244af0feb0a3d3ad367acd",
       network_id: "*" // Match any network id
